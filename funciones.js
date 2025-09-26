f284e56135e1445dcf1a72696d69d20a7eae7f60
NadinContreras <90220313+NadinContreras@users.noreply.github.com>
2025-09-26 15:55:52 -0500

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -213,7 +213,7 @@ function ordenarPorNumero() {
 }
 
 function actualizarContador(valor) {
-  var texto = valor === 0 ? "No hay contratos visibles" : "Mostrando " + valor + " contratos";
+  var texto = valor === 0 ? "No hay contratos visibles" : " " + valor + " contratos";
   var contador = document.getElementById("contador");
   if (contador) contador.innerText = texto;
 }
@@ -287,7 +287,7 @@ function cambiarAnio() {
   const contenedor = document.getElementById('contenedor-tabla');
   const subtitulo = document.querySelector('.subtitle');
 
-  subtitulo.textContent = `Mostrando contratación del año ${anio}`;
+  subtitulo.textContent = `Mostrando contratación del año ${anio} |`;
 
   fetch(`contratos_${anio}.html`)
     .then(res => res.text())
@@ -381,6 +381,7 @@ function actualizarTiempoEnTabla() {
     }
   }
 }
+
 
 
 
